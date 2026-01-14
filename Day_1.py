f83a6c354de3e67e7e4abd575c3b61087d7967ee
juanmorales879 <juanmorales.marcano@gmail.com>
2026-01-13 22:17:39 -0400

    day 1

---
+++ Day_1.py
@@ -0,0 +1,4 @@
+print("Hello and welcome to the band name generator: ")
+city = input("What city did you grew up in: ")
+pet = input("What is the name of your pet: ")
+print("Your band name could be: " + pet + " " + city)
